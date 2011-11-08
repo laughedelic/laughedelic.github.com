432bd4831a1c6cae5562eb3fa354a944bd999964
laughedelic <laughedelic@gmail.com>
2011-11-09 01:00:32 +0400

    there should be no empty figcaption

--- plugins/backtick_code_block.rb
+++ plugins/backtick_code_block.rb
@@ -19,7 +19,9 @@ module BacktickCodeBlock
         @caption = "<figcaption><span>#{$2}</span><a href='#{$3}'>#{$4 || 'link'}</a></figcaption>"
       elsif @options =~ LangCaption
         @lang = $1
-        @caption = "<figcaption><span>#{$2}</span></figcaption>"
+        if $2 != nil
+            @caption = "<figcaption><span>#{$2}</span></figcaption>"
+        end
       end
 
       if str.match(/\A( {4}|\t)/)
